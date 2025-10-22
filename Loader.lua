fdaeaae3e3f8722e73842634b7d35f37abaa0be2
hajibeza <74174244+hajibeza@users.noreply.github.com>
2025-10-22 22:36:03 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,13 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
+local games = {
+    [2753915549] = 'https://raw.githubusercontent.com/hajibeza/Main/refs/heads/main/RIPPER_V5.lua, -- BF
+    [4442272183] = 'https://raw.githubusercontent.com/hajibeza/Main/refs/heads/main/RIPPER_V5.lua', -- BF
+    [7449423635] = 'https://raw.githubusercontent.com/hajibeza/Main/refs/heads/main/RIPPER_V5.lua', -- BF
+}
+
+if game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
+    loadstring(game:HttpGet(games[game.PlaceId]))()
+end
